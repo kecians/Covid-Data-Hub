2d04699e8e2df2f75bd66bac4574ef33078d974c
shubhamtrip16 <shubhamtrip16@gmail.com>
2021-05-12 20:11:16 +0530

    Login funcitonality initialised

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,12 +1,15 @@
+
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:patient_status_app/Components/RoundButton.dart';
+import 'package:patient_status_app/Networking/Networking.dart';
+import 'package:patient_status_app/Screens/Nurses/NurseLogin.dart';
 import 'package:patient_status_app/Screens/Register.dart';
 import 'package:patient_status_app/Utilities/constants.dart';
 import 'package:patient_status_app/Components/TitleHead.dart';
-
-import 'Nurses/NurseLogin.dart';
 class Home extends StatefulWidget {
   static const String id = 'HomeScreen';
   @override
@@ -14,6 +17,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Networking instance = Networking();
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  String errortext ="";
+  bool isLogin = false;
+  @override
+  void initState() {
+    super.initState();
+
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -38,30 +51,57 @@ class _HomeState extends State<Home> {
 
                 children:[
                   TitleHead(),
-                  SizedBox(height: 100),
+                  SizedBox(height: 80),
                   Column(
                       children: [
-
+                        Text(errortext,style: TextStyle(color: Colors.red),),
+                        SizedBox(height: 10,),
                         Container(
                           width: 260,
                           child: TextField(
+
+                            controller: _usernameController,
+
                             style: TextStyle(color: Colors.black),
-                            onChanged: (value){},
-                            decoration: kInputDecorantion.copyWith(hintText: 'Enter your Username'),
+                            decoration: kInputDecorantion.copyWith(hintText: "Enter Your Username"),
                           ),
                         ),
                         SizedBox(height: 30),
                         Container(
                           width: 260,
                           child: TextField(
+                            controller: _passwordController,
                             style: TextStyle(color: Colors.black),
-                            onChanged: (value){},
                             decoration: kInputDecorantion.copyWith(hintText: 'Enter your password',),
                           ),
                         ),
                         SizedBox(height: 100),
                         RoundButton(color: Color(0XFFD5031A8D),text: "Login", textColor: Colors.white,
-                          onpress: (){ Navigator.pushNamed(context, NurseLogin.id); },height: 50,width: 260,),
+                          onpress: ()async{
+                            var statuscode = await instance.signin(_usernameController.text, _passwordController.text);
+
+                            if(statuscode == 200)
+                              {
+                                 Navigator.pushNamedAndRemoveUntil(context, NurseLogin.id, (route) => false);
+
+                              }
+                             else
+                               {
+                                 if(_usernameController.text.length <=4 || _passwordController.text.length<3)
+                                   {
+                                     errortext = "Entery too short";
+                                   }
+                                 else{
+                                   errortext = "Invalid User";
+                                 }
+                                 setState(() {
+                                   errortext;
+                                   _usernameController.text = "";
+                                   _passwordController.text ="";
+                                 });
+                               }
+                            }
+                          ,height: 50,width: 260,),
                         SizedBox(height: 10,),
                         Text('Or',style: TextStyle(fontWeight: FontWeight.w900,fontSize: 16),),
                         SizedBox(height: 10,),
